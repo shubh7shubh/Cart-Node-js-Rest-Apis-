3d64f3534f76030fb559c444d8739e068f0d0a49
shubham <khobragade.shubham@gmail.com>
2024-05-15 12:11:21 +0530

    added the routes

---
+++ routes/CartRoutes.js
@@ -0,0 +1,6 @@
+// Cart Routes
+router.post("/addtocart", authToken, addToCartController)
+router.get("/countAddToCartProduct", authToken, countAddToCartProduct)
+router.get("/view-card-product", authToken, addToCartViewProduct)
+router.post("/update-cart-product", authToken, updateAddToCartProduct)
+router.post("/delete-cart-product", authToken, deleteAddToCartProduct)
